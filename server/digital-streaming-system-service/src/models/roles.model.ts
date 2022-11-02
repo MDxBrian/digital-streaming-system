04cf2ab2d1ce3fa426d07bcc77e6671ae16aca15
Mark Daniell Brian <markbtiane@gmail.com>
2022-11-02 12:04:36 +0800

    initial commit

---
+++ server/digital-streaming-system-service/src/models/roles.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Roles extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role_name: string;
+
+  @property({
+    type: 'string',
+  })
+  role_description?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Roles>) {
+    super(data);
+  }
+}
+
+export interface RolesRelations {
+  // describe navigational properties here
+}
+
+export type RolesWithRelations = Roles & RolesRelations;
